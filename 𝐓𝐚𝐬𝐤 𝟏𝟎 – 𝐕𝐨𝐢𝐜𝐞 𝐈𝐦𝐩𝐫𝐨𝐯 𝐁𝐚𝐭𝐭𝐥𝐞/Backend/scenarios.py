2e55f5b5544d77c037da9a3654348cd91b8edc40
Vallepu Vamsi <vallepuvamsi2003@gmail.com>
2025-12-01 09:05:32 +0530

    Add files via upload

---
+++ 𝐓𝐚𝐬𝐤 𝟏𝟎 – 𝐕𝐨𝐢𝐜𝐞 𝐈𝐦𝐩𝐫𝐨𝐯 𝐁𝐚𝐭𝐭𝐥𝐞/Backend/scenarios.py
@@ -0,0 +1,9 @@
+SCENARIOS = [
+    "You are a barista who has to tell a customer that their latte is actually a portal to another dimension.",
+    "You are a time-travelling tour guide explaining modern smartphones to someone from the 1800s.",
+    "You are a restaurant waiter who must calmly tell a customer that their order has escaped the kitchen.",
+    "You are trying to return an obviously cursed object (a glowing skull) to a very skeptical shop owner.",
+    "You are a cat trying to explain quantum physics to a dog using only meows and gestures (translate the meows to English).",
+    "You are an alien spy trying to blend in at a human barbecue but you misunderstand what a 'hot dog' is.",
+    "You are a GPS navigation system that is going through a messy breakup with the driver."
+]
